5c77d0b12ede5b290aa607a83416d43c15325f3a
kobyou <kobyou@126.com>
2020-07-31 12:07:43 +0800

    update code

--- StockReport/StockReport.py
+++ StockReport/StockReport.py
@@ -32,7 +32,7 @@ class StockReport(object):
         self.startdate = n_days.strftime('%Y%m%d')
         self.showLine = showline
         #print (self.startdate)
-        
+
         if not os.path.exists("./report"):
             os.mkdir("./report")
         
@@ -186,101 +186,108 @@ class StockReport(object):
         return stock_code
 
     def draw_k_line(self,stock_str):
+        try:
         # 导入数据
-        symbol = self.parse_stock_code(stock_str)
-        period = 500
-        #df = self.import_csv(symbol)[-period:]
-        df = self.import_ts_data(symbol)[-period:]
-        # 设置基本参数
-        # type:绘制图形的类型，有candle, renko, ohlc, line等
-        # 此处选择candle,即K线图
-        # mav(moving average):均线类型,此处设置7,30,60日线
-        # volume:布尔类型，设置是否显示成交量，默认False
-        # title:设置标题
-        # y_label:设置纵轴主标题
-        # y_label_lower:设置成交量图一栏的标题
-        # figratio:设置图形纵横比
-        # figscale:设置图形尺寸(数值越大图像质量越高)
-        kwargs = dict(
-            type='candle', 
-            mav=(7, 30, 60), 
-            volume=True, 
-            title='\nA_stock %s candle_line' % (stock_str),    
-            ylabel='OHLC Candles', 
-            ylabel_lower='Shares\nTraded Volume', 
-            figratio=(15, 10), 
-            figscale=1.1)
-        
-        # 设置marketcolors
-        # up:设置K线线柱颜色，up意为收盘价大于等于开盘价
-        # down:与up相反，这样设置与国内K线颜色标准相符
-        # edge:K线线柱边缘颜色(i代表继承自up和down的颜色)，下同。详见官方文档)
-        # wick:灯芯(上下影线)颜色
-        # volume:成交量直方图的颜色
-        # inherit:是否继承，选填
-        mc = mpf.make_marketcolors(
-            up='red', 
-            down='green', 
-            edge='i', 
-            wick='i', 
-            volume='in', 
-            inherit=True)
+            symbol = self.parse_stock_code(stock_str)
+            period = 500
+            #df = self.import_csv(symbol)[-period:]
+            df = self.import_ts_data(symbol)[-period:]
+            # 设置基本参数
+            # type:绘制图形的类型，有candle, renko, ohlc, line等
+            # 此处选择candle,即K线图
+            # mav(moving average):均线类型,此处设置7,30,60日线
+            # volume:布尔类型，设置是否显示成交量，默认False
+            # title:设置标题
+            # y_label:设置纵轴主标题
+            # y_label_lower:设置成交量图一栏的标题
+            # figratio:设置图形纵横比
+            # figscale:设置图形尺寸(数值越大图像质量越高)
+            kwargs = dict(
+                type='candle', 
+                mav=(7, 30, 60), 
+                volume=True, 
+                title='\nA_stock %s candle_line' % (stock_str),    
+                ylabel='OHLC Candles', 
+                ylabel_lower='Shares\nTraded Volume', 
+                figratio=(15, 10), 
+                figscale=1.1)
             
-        # 设置图形风格
-        # gridaxis:设置网格线位置
-        # gridstyle:设置网格线线型
-        # y_on_right:设置y轴位置是否在右
-        s = mpf.make_mpf_style(
-            gridaxis='both', 
-            gridstyle='-.', 
-            y_on_right=False, 
-            marketcolors=mc)
+            # 设置marketcolors
+            # up:设置K线线柱颜色，up意为收盘价大于等于开盘价
+            # down:与up相反，这样设置与国内K线颜色标准相符
+            # edge:K线线柱边缘颜色(i代表继承自up和down的颜色)，下同。详见官方文档)
+            # wick:灯芯(上下影线)颜色
+            # volume:成交量直方图的颜色
+            # inherit:是否继承，选填
+            mc = mpf.make_marketcolors(
+                up='red', 
+                down='green', 
+                edge='i', 
+                wick='i', 
+                volume='in', 
+                inherit=True)
+                
+            # 设置图形风格
+            # gridaxis:设置网格线位置
+            # gridstyle:设置网格线线型
+            # y_on_right:设置y轴位置是否在右
+            s = mpf.make_mpf_style(
+                gridaxis='both', 
+                gridstyle='-.', 
+                y_on_right=False, 
+                marketcolors=mc)
+                
+            # 设置均线颜色，配色表可见下图
+            # 建议设置较深的颜色且与红色、绿色形成对比
+            # 此处设置七条均线的颜色，也可应用默认设置
+            mpl.rcParams['axes.prop_cycle'] = cycler(
+                color=['dodgerblue', 'deeppink', 
+                'navy', 'teal', 'maroon', 'darkorange', 
+                'indigo'])
             
-        # 设置均线颜色，配色表可见下图
-        # 建议设置较深的颜色且与红色、绿色形成对比
-        # 此处设置七条均线的颜色，也可应用默认设置
-        mpl.rcParams['axes.prop_cycle'] = cycler(
-            color=['dodgerblue', 'deeppink', 
-            'navy', 'teal', 'maroon', 'darkorange', 
-            'indigo'])
+            # 设置线宽
+            mpl.rcParams['lines.linewidth'] = .5
         
-        # 设置线宽
-        mpl.rcParams['lines.linewidth'] = .5
-     
-        # 图形绘制
-        # show_nontrading:是否显示非交易日，默认False
-        # savefig:导出图片，填写文件名及后缀
-        # mpf.plot(df, 
-        #     **kwargs, 
-        #     style=s, 
-        #     show_nontrading=False,
-        #     savefig='A_stock-%s %s_candle_line'
-        #     % (symbol, period) + '.jpg',
-        #     block = False)
-        mpf.plot(df, 
-            **kwargs, 
-            style=s, 
-            show_nontrading=False,
-            block = False)
-        plt.rcParams['font.sans-serif'] = ['FangSong', 'KaiTi']  
-        plt.savefig('./report/A_stock-%s %s_candle_line.jpg'% (symbol, period))
+            # 图形绘制
+            # show_nontrading:是否显示非交易日，默认False
+            # savefig:导出图片，填写文件名及后缀
+            # mpf.plot(df, 
+            #     **kwargs, 
+            #     style=s, 
+            #     show_nontrading=False,
+            #     savefig='A_stock-%s %s_candle_line'
+            #     % (symbol, period) + '.jpg',
+            #     block = False)
+            mpf.plot(df, 
+                **kwargs, 
+                style=s, 
+                show_nontrading=False,
+                block = False)
+            plt.rcParams['font.sans-serif'] = ['FangSong', 'KaiTi']  
+            plt.savefig('./report/A_stock-%s %s_candle_line.jpg'% (stock_str, period))
+        except Exception as e:
+            print(e)
         #     % (symbol, period))     
         #plt.show()
 
     def run(self):
-        stock_code = '上汽集团(600104)'
-        self.draw_k_line(stock_code)
-        dicdata = self.show_stock_report(self.stockRange)
-        if self.showLine == True:
-            for i in range(0,self.stockRange):
-                #stock_tup = dicdata[0]
-                stock_code = dicdata[i][0]
-                self.draw_k_line(stock_code)
-        plt.show()
-
+        try:
+            stock_code = '上汽集团(600104)'
+            self.draw_k_line(stock_code)
+            dicdata = self.show_stock_report(self.stockRange)
+            if self.showLine == True:
+                for i in range(0,self.stockRange):
+                    #stock_tup = dicdata[0]
+                    stock_code = dicdata[i][0]
+                    self.draw_k_line(stock_code)
+            plt.show()
+        except Exception as e:
+            print(e)
+ 
 if __name__ == "__main__":
     pass
     stock_range = 20
-    show_line = 0
+    show_line = 1
     stockReport = StockReport(stock_range,show_line) 
     stockReport.run()
+    
